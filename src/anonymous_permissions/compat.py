aed9f7b088bd1f6949521d253e4bee37fe7610de
John Vandenberg <jayvdb@gmail.com>
2020-05-25 11:12:58 +0700

    compat: Support Django 3

--- src/anonymous_permissions/compat.py
+++ src/anonymous_permissions/compat.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals
 import django
 import six
 
+DJANGO3 = django.VERSION[0] == 3
 DJANGO2 = django.VERSION[0] == 2
 DJANGO1 = django.VERSION[0] == 1
 DJANGO19 = django.VERSION[0:1] == (1, 9)
@@ -11,7 +12,7 @@ DJANGO110 = django.VERSION[0:1] == (1, 10)
 DJANGO111 = django.VERSION[0:1] == (1, 11)
 DJANGO_PRE_11 = django.VERSION[0:2] < (1, 11)
 
-if DJANGO2:
+if DJANGO2 or DJANGO3:
     def is_anonymous(user):
         return user.is_anonymous
 
